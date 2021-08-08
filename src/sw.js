3ae63c0cc85094480b35150ed5cfc6aee92bf1da
Vikas <vikasmaur10@gmail.com>
2021-08-08 19:22:38 +0530

    changes to sw,manifest icons. Added in app install prompt. Changed some homepage design.Also added about page

--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,36 @@
-self.addEventListener("install", (e) => {});
-self.addEventListener("activate", (e) => {});
-self.addEventListener("fetch", (e) => {});
+const staticCacheName = "site-static-v1";
+const assets = [
+  "https://fonts.googleapis.com",
+  "https://fonts.gstatic.com",
+  "https://fonts.googleapis.com/css2?family=Nunito&display=swap",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cacheRes) => {
+      return cacheRes || fetch(e.request);
+    })
+  );
+});
